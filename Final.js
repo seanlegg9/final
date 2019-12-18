e639ff5e8d91b9fbe4f24b5423b2a6685c8fccea
seanlegg9 <slegg@gmail.com>
2019-12-18 08:22:05 -0700

    Complete

--- Final.js
+++ Final.js
@@ -1,47 +1,80 @@
 let p = 0;
 let order = document.forms.order;
 let size = document.forms.order.elements.size;
-// let pep = document.forms.order.elements.pep;
-// let itasau = document.forms.order.elements.itasau;
-// let slisau = document.forms.order.elements.slisau;
-// let beef = document.forms.order.elements.beef;
-// let sal = document.forms.order.elements.sal;
-// let chi = document.forms.order.elements.chi;
-// let bac = document.forms.order.elements.bac;
-// let ham = document.forms.order.elements.ham;
-// let philsteak = document.forms.order.elements.philsteak;
-// let mush = document.forms.order.elements.mush;
-// let oni = document.forms.order.elements.oni;
-// let grepep = document.forms.order.elements.grepep;
-// let blaoli = document.forms.order.elements.blaoli;
-// let jal = document.forms.order.elements.jal;
-// let rrp = document.forms.order.elements.rrp;
-// let tom = document.forms.order.elements.tom;
-// let greoli = document.forms.order.elements.greoli;
-// let pine = document.forms.order.elements.pine;
-// let cheche = document.forms.order.elements.cheche;
-// let provche = document.forms.order.elements.provche;
-// let feta = document.forms.order.elements.feta;
-// let sauce = document.forms.order.elements.sauce;
-
+Pepperoni.style.visibility='hidden'
+SausageSlice.style.visibility='hidden'
+Sausage.style.visibility='hidden'
+Beef.style.visibility='hidden'
+Salami.style.visibility='hidden'
+Chicken.style.visibility='hidden'
+Bacon.style.visibility='hidden'
+Pineapple.style.visibility='hidden'
+Olives.style.visibility='hidden'
+order.oninput = function() {
+    if (document.querySelector('.itasau:checked') !== null) { // New: Lets me chcek if a specific object is in a class. In this case, if pepperoni is checked.
+        Sausage.style.visibility='visible'
+    } else {
+        Sausage.style.visibility='hidden'
+    }
+    if (document.querySelector('.slisau:checked') !== null) { 
+        SausageSlice.style.visibility='visible'
+    } else {
+        SausageSlice.style.visibility='hidden'
+    }
+    if (document.querySelector('.beef:checked') !== null) { 
+        Beef.style.visibility='visible'
+    } else {
+        Beef.style.visibility='hidden'
+    }
+    if (document.querySelector('.sal:checked') !== null) { 
+        Salami.style.visibility='visible'
+    } else {
+        Salami.style.visibility='hidden'
+    }
+    if (document.querySelector('.chi:checked') !== null) { 
+        Chicken.style.visibility='visible'
+    } else {
+        Chicken.style.visibility='hidden'
+    }
+    if (document.querySelector('.bac:checked') !== null) {
+        Bacon.style.visibility='visible'
+    } else {
+        Bacon.style.visibility='hidden'
+    }
+    if (document.querySelector('.pine:checked') !== null) { 
+        Pineapple.style.visibility='visible'
+    } else {
+        Pineapple.style.visibility='hidden'
+    }
+    if (document.querySelector('.oli:checked') !== null) { 
+        Olives.style.visibility='visible'
+    } else {
+        Olives.style.visibility='hidden'
+    }
+    if (document.querySelector('.pep:checked') !== null) {  // New: Lets me chcek if a specific object is in a class. In this case, if pepperoni is checked.
+        Pepperoni.style.visibility='visible'
+    } else {
+        Pepperoni.style.visibility='hidden'
+    }
+}
 order.onsubmit = function(event) {
     event.preventDefault()
-    if (size = "small") {
+    if (size.value == "small") {
         p = p + 5.99
-    } else if (size = "medium") {
+    } else if (size.value == "medium") {
         p = p + 7.99
     } else {
         p = p + 10.99
-    }
-    if (document.querySelector('.sauce:checked') !== null) { // Lets me chcek if a specific object is in a class. In this case, if sauce is checked.
-        p = p + 0.25
     }
     let c = document.querySelectorAll("input:checked").length // New: This allows me to easily see how many objects are in a certain style. In this case, boxes that are checked.
     c = c * 0.5
     let pricetaxless = p + c;
     let tax = pricetaxless * .029
-    let fullprice = pricetaxless + tax
-    console.log(pricetaxless)
-    console.log(tax)
-    console.log(fullprice)
+    tax = tax.toFixed(2) // New: Rounds numbers for me. In this case, the tax.
+    tax = parseFloat(tax) // New: When the number is rounded, it becomes a string. This command makes it into a float again.
+    let fullprice = pricetaxless + tax 
+    fullprice = fullprice.toFixed(2)
+    pizzataxless.append(pricetaxless)
+    pizzatax.append(tax)
+    pizzafull.append(fullprice)
 }
